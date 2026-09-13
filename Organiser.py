79b551131be7d3cbe209a18130562e60a3d85844
Shivam-scripts <shivam.devwork@gmail.com>
2026-09-14 01:17:36 +0530

    CLI File Organiser V2 : used functions

--- Organiser.py
+++ Organiser.py
@@ -1,45 +1,52 @@
 from pathlib import Path
 import shutil
 
-selected_folder = Path(input("Enter folder path: "))
 folder_type = [
-    {
-        "folderName" : "Images",
-        "folderType" : [".png",".jpeg",".jpg"]
-    },
-    {
-        "folderName" : "Videos",
-        "folderType" : [".mp4",".mov",".avi",".mkv",".webm"]
-    },
-    {
-        "folderName" : "Documents",
-        "folderType" : [".docx",".txt"]
-    },
-]
+            {
+                "folderName" : "Images",
+                "folderType" : [".png",".jpeg",".jpg"]
+            },
+            {
+                "folderName" : "Videos",
+                "folderType" : [".mp4",".mov",".avi",".mkv",".webm"]
+            },
+            {
+                "folderName" : "Documents",
+                "folderType" : [".docx",".txt"]
+            },
+        ]
 
-if not selected_folder.exists():
-    print("Selected folder does not exist")
+def get_folder():
+    folder_path = Path(input("Enter folder path: "))
+    if not folder_path.exists() or not folder_path.is_dir():
+        return 
+    return folder_path
 
-else:
-    skip_files=0
-    for entry in selected_folder.iterdir():
-        if entry.is_file():
-            file_extension_type = entry.suffix.lower()
-            folderName = "Others"
-            
+def get_extension(item):
+    return item.suffix.lower()
 
-            if file_extension_type=="":
-                skip_files +=1
-                continue
+def get_category(file_extension):
+    for category in folder_type:
+        if file_extension in category["folderType"]:
+            return category["folderName"]
+    return "Others"
 
-            for category in folder_type:
-                if file_extension_type in category["folderType"]:
-                    folderName = category["folderName"]
-                    break
+def move_file(curr_file,dir_folder):
+    dir_folder.mkdir(parents=True, exist_ok=True)
+    shutil.move(curr_file,dir_folder)
+
+def main():
+    selected_folder=get_folder()
+    if selected_folder is not None:
+        for item in selected_folder.iterdir():
+            if item.is_file():
+                file_extension = get_extension(item)
+                file_category = get_category(file_extension)
+
                 
-            transfer_dir = selected_folder / folderName
+                dir_folder = selected_folder/file_category
 
-            transfer_dir.mkdir(parents=True, exist_ok=True)
-            shutil.move(entry, transfer_dir)
-
-    print(f"Skipped files: {skip_files}")
+                move_file(item,dir_folder)
+    else:
+        print("Invalid Paths")
+main()
